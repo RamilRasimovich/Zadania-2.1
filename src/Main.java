e111e5ca7e4b1ec38cec44308de97779bbf7548d
ramil506cx test <ramil506cx@icloud.com>
2023-03-29 22:01:48 +0500

    Gitignore added

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-       int ticketprice = 13676;
-       int pricepermile = 20;
-       int bonuspermile = 1;
-       int amountofbonus = ticketprice / pricepermile * bonuspermile;
+        int ticketprice = 13676;
+        int pricepermile = 20;
+        int bonuspermile = 1;
+        int amountofbonus = ticketprice / pricepermile * bonuspermile;
         System.out.println(amountofbonus = ticketprice / pricepermile * bonuspermile);
 
 
-
-   }
-
     }
+
+}
 
